ecca0cd34978f803fcd4bd6afcf70771fb55df75
Lucas Malta <lucas50xd@hotmail.com>
2018-06-15 00:27:04 -0300

    adds intro

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,13 +3,18 @@ import {
     Link
 } from 'react-router-dom'
 
+import './Home.css';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                hi
-                <Link to="/gamid">Gamid!</Link>
+            <div className="gamid-main">
+                <h1 className="gamid-title" >Bem vindo ao Gamid!</h1>
+                <p className="gamid-content">O Gamid é um framework que utiliza de Vanilla JS para facilitar
+                    a criação de jogos e simulações bidimensionais. Através das bibliotecas
+                    Graphics, Time, Controller, Physics e Audio é possível construir
+                    aplicações que utilizam de um canvas HTML numa velocidade impressionante.
+                </p>
             </div>
         );
     }
