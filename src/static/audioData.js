468d7fea8d01111aa3d5ab19428f6e4788a676fe
guilacerda <guiglacerda@gmail.com>
2018-06-18 20:39:04 -0300

    Adds the audio description
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- src/static/audioData.js
+++ src/static/audioData.js
@@ -1,19 +1,21 @@
 export const title = "Audio"
-export const links = [] // não precisa!
+export const links = []
 
-export const content = 
+export const content =
 `
-explicação aqui
-
+Audio é a biblioteca responsável por atribuir som à objetos. Ela é encarregada
+de realizar todos os métodos descritos posteriormente, do ínicio ao fim da construção
+de um áudio.
 `
-
 
 export const attributeList = [
-    {attr: "atributo 1", description: "Atributos aqui!"},
-    {attr: "atributo 2", description: "woo"}
+    {attr: "play(path_do_som)", description: "String : Inicializa o áudio requerido."},
+    {attr: "pause(path_do_som)", description: "String : Pausa o áudio requerido e quando der o play() novamente, volta aonde tinha pausado."},
+    {attr: "loop(path_do_som)", description: "String : Faz com que o áudio funcione em loop. Quando acabar o áudio, inicia novamente."},
+    {attr: "stop(path_do_som)", description: "String : Finaliza a música."}
 ]
 
-export const example = 
+export const example =
 `
 console.log("exemplo de instanciação aqui!");
 
