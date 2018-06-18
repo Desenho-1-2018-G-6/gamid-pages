15634bcdb623e589683ec618c4bea08841e3b0f0
guilacerda <guiglacerda@gmail.com>
2018-06-18 20:55:22 -0300

    Adds the audio example
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- src/static/audioData.js
+++ src/static/audioData.js
@@ -17,6 +17,46 @@ export const attributeList = [
 
 export const example =
 `
-console.log("exemplo de instanciação aqui!");
+let canvasInstance = new gamid.graphics.Canvas(800, 800, 'cyan');
+let canvas = canvasInstance.element;
+
+// Cria um objeto do tipo Quadrado com seus respectivos atributos
+let base = new gamid.graphics.Square(100, 100, 350, 350, 1, 'black');
+
+// Adiciona um ou mais tipo de controles via Keyboard ao objeto instanciado (base)
+let baseController = new gamid.controller.Keyboard(base, [87, 83]);
+
+// Adiciona uma ação à tecla 'w'.
+keyboard.setOnKeyDown(87, function(){
+		square.speedY = -500;
+});
+
+// Adiciona uma ação à tecla 's'.
+keyboard.setOnKeyDown(83, function(){
+		square.speedY = 500;
+});
+
+// Cria a lista de áudios que deseja inserir no seu jogo.
+// Pode estar em qualquer formato de áudio suportado pelo navegador (wav, mp3 ...).
+let audioList = [
+    "caminho_da_musica_1",
+]
+
+// Chama o construtor do áudio que deseja, passando o objeto e a lista de audios.
+// O construtor irá adicionar e criar os áudios dentro do seu jogo.
+let audio = new gamid.audio.Audio(base, audioList);
+
+canvasInstance.addObject(base);
+
+// Utiliza da método play() para iniciar o áudio.
+// Este método não reproduz novamente o áudio quando esgotado seu tempo.
+audio.play("caminho_da_musica_1");
+
+// Caso queira usar o áudio em loop pode utilizar do método loop().
+// Este método só irá parar de reproduzir o áudio quando chamada o
+// método de pause("caminho_da_musica_1") ou então de stop("caminho_da_musica_1").
+audio.loop("caminho_da_musica_1");
+
+document.body.appendChild(canvas);
 
 `
