79e14cec31f9f203511b38967bd9db721b69d0d6
guilacerda <guiglacerda@gmail.com>
2018-06-18 21:04:44 -0300

    Increments e translates squareData

--- src/static/squareData.js
+++ src/static/squareData.js
@@ -3,11 +3,11 @@ export const links = [
                     {title:"Retornar", link:"/gamid-pages/graphics"}
                     ]
 
-export const content = 
+export const content =
 `
-Square é o exemplo base da instanciação de um baseObject. Nele, pode-se 
+Square é o exemplo base da instanciação de um baseObject. Nele, pode-se
 utilizar de todos os decorators provindos para baseObject. Esses decorators
-podem ser melhor entendidos nas bibliotecas de Controller e Physics. Além disso, 
+podem ser melhor entendidos nas bibliotecas de Controller e Physics. Além disso,
 por se tratar de uma classe concreta de baseObject, todos os atributos de baseObject
 foram herdados.
 `
@@ -16,16 +16,18 @@ export const attributeList = [
     {attr: "speedX", description: "Massa do objeto instanciado"},
     {attr: "speedY", description: "Altura do objeto instanciado"},
     {attr: "color", description: "Largura do objeto instanciado"}
-     
+
 ]
 
-export const example = 
+export const example =
 `
-//creates canvas
-let canvasInstance = new gamid.graphics.Canvas(800, 800, 'purple'); 
+// Cria o canvas
+let canvasInstance = new gamid.graphics.Canvas(800, 800, 'purple');
 
-//creates baseObject - square
-let square = new gamid.graphics.Square(50, 50, 0, 0, 50, 'blue'); 
-// inserts square on canvas
-canvasInstance.addObject(square); 
+// Cria o objeto square com seus respectivos atributos.
+// Width, height, posição no eixo X, posição no eixo Y, massa e cor, respectivamente.
+let square = new gamid.graphics.Square(50, 50, 0, 0, 50, 'blue');
+
+// Insere o objeto criado no Canvas para então ser renderizado.
+canvasInstance.addObject(square);
 `
